24836bc59102c7f57f02d90c1e8c67b1476a7aa6
Mark Oldham <mark.oldham1@btinternet.com>
2024-03-08 18:18:31 +0000

    Generate empty grid.

--- super_sudoku/super_sudoku.cpp
+++ super_sudoku/super_sudoku.cpp
@@ -5,7 +5,15 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            std::cout << "_ ";
+        }
+
+        std::cout << std::endl;
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
