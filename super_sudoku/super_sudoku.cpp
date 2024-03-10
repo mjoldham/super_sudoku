b10076df7ae232bc50932f63b57526ecfb94963f
Mark Oldham <mark.oldham1@btinternet.com>
2024-03-10 20:06:21 +0000

    Added row, column, and subgrid tallies.

--- super_sudoku/super_sudoku.cpp
+++ super_sudoku/super_sudoku.cpp
@@ -5,6 +5,8 @@
 #include <random>
 #include <algorithm>
 #include <cstdlib>
+#include <bitset>
+
 using namespace std;
 
 int RandomRange(int n)
@@ -25,6 +27,9 @@ int RandomRange(int n)
 struct Grid9x9
 {
 	int values[9][9];
+	Grid9x9() : values{ 0 }
+	{
+	}
 };
 
 Grid9x9 GenerateGrid()
@@ -42,19 +47,62 @@ Grid9x9 GenerateGrid()
 	return grid;
 }
 
+struct NumberTally
+{
+	int rowCount[9][9], colCount[9][9], subCount[9][9];
+	NumberTally() : rowCount{ 0 }, colCount{ 0 }, subCount{ 0 }
+	{
+	}
+};
+
+NumberTally CountRepeats(Grid9x9 inGrid)
+{
+	// Checks the rows, columns, and 3x3 subgrids.
+	NumberTally tally;
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			int value = inGrid.values[i][j];
+			if (value == 0)
+			{
+				continue;
+			}
+
+			tally.rowCount[i][value - 1]++;
+			tally.colCount[j][value - 1]++;
+			tally.subCount[(i / 3) * 3 + j / 3][value - 1]++; // Some integer truncation trickery!
+		}
+	}
+
+	return tally;
+}
+
 void DrawGrid(const Grid9x9& grid)
 {
-	for (int j1 = 0; j1 < 3; j1++)
+	for (int i = 0; i < 23; i++)
 	{
-		for (int j2 = 0; j2 < 3; j2++)
+		cout << " ";
+	}
+
+	for (int i = 0; i < 9; i++)
+	{
+		cout << " " << i + 1 << ":";
+	}
+	cout << endl;
+	
+	NumberTally tally = CountRepeats(grid);
+	for (int i1 = 0; i1 < 3; i1++)
+	{
+		for (int i2 = 0; i2 < 3; i2++)
 		{
-			int j = j1 * 3 + j2;
+			int i = i1 * 3 + i2;
 			cout << "  ";
-			for (int i1 = 0; i1 < 3; i1++)
+			for (int j1 = 0; j1 < 3; j1++)
 			{
-				for (int i2 = 0; i2 < 3; i2++)
+				for (int j2 = 0; j2 < 3; j2++)
 				{
-					int value = grid.values[i1 * 3 + i2][j];
+					int value = grid.values[i][j1 * 3 + j2];
 					if (value == 0)
 					{
 						cout << "_ ";
@@ -66,7 +114,44 @@ void DrawGrid(const Grid9x9& grid)
 				}
 				cout << " ";
 			}
+
+			cout << "\b";
+			for (int k = 0; k < 9; k++)
+			{
+				cout << "  " << tally.rowCount[i][k];
+			}
 			cout << endl;
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < 23; i++)
+	{
+		cout << " ";
+	}
+
+	for (int i = 0; i < 9; i++)
+	{
+		cout << " s" << i << ":";
+	}
+	cout << endl;
+
+	for (int k = 0; k < 9; k++)
+	{
+		cout << k + 1 << ":";
+		for (int j1 = 0; j1 < 3; j1++)
+		{
+			for (int j2 = 0; j2 < 3; j2++)
+			{
+				cout << tally.colCount[j1 * 3 + j2][k] << " ";
+			}
+			cout << " ";
+		}
+
+		cout << "\b";
+		for (int ij = 0; ij < 9; ij++)
+		{
+			cout << "   " << tally.subCount[ij][k];
 		}
 		cout << endl;
 	}
