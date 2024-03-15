5b74888c8098acb701e683bdb54e95da85f19ee2
Mark Oldham <mark.oldham1@btinternet.com>
2024-03-15 01:24:29 +0000

    Refactored, better OOP stuff.

--- super_sudoku/super_sudoku.cpp
+++ super_sudoku/super_sudoku.cpp
@@ -12,6 +12,10 @@
 using namespace std;
 using namespace chrono;
 
+class ConstraintMatrix;
+class Grid9x9;
+bool XSolver(ConstraintMatrix& matrix, Grid9x9& grid, int timeOut = 300, int pos = 0, int r1 = 0);
+
 int RandomRange(int n)
 {
 	n = clamp(n, 0, RAND_MAX);
@@ -27,63 +31,9 @@ int RandomRange(int n)
 	return r;
 }
 
-struct Grid9x9
-{
-	int values[9][9];
-	Grid9x9() : values{ 0 }
-	{
-	}
-};
-
-struct NumberTally
-{
-	int rowCount[9][9], colCount[9][9], subCount[9][9];
-	NumberTally() : rowCount{ 0 }, colCount{ 0 }, subCount{ 0 }
-	{}
-};
-
-Grid9x9 GenerateGrid()
-{
-	srand(time(NULL));
-	Grid9x9 grid;
-	NumberTally tally;
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
-		{
-			int value = RandomRange(10);
-			if (value != 0)
-			{
-				int valIndex = value - 1;
-				int ij = (i / 3) * 3 + j / 3;
-				if (tally.rowCount[i][valIndex] > 0 ||
-					tally.colCount[j][valIndex] > 0 ||
-					tally.subCount[ij][valIndex] > 0)
-				{
-					value = 0;
-				}
-				else
-				{
-					tally.rowCount[i][valIndex] = tally.colCount[j][valIndex] = tally.subCount[ij][valIndex] = 1;
-				}
-			}
-
-			grid.values[i][j] = value;
-		}
-	}
-
-	return grid;
-}
-
 inline int GetBoxIndex(int i, int j)
 {
 	return (i / 3) * 3 + j / 3; // Some integer truncation trickery!
-}
-
-inline void GetGridIndices(int inPos, int& outI, int& outJ)
-{
-	outJ = inPos % 9;
-	outI = (inPos - outJ) / 9;
 }
 
 inline int GetRowConstraint(int i, int cand = 0)
@@ -101,124 +51,53 @@ inline int GetBoxConstraint(int i, int j, int cand = 0)
 	return 81 * 3 + GetBoxIndex(i, j) * 9 + cand;
 }
 
-NumberTally CountRepeats(Grid9x9 inGrid)
+int iter = 0, totalIter = 0, incr = 0, incrSize = 1000;
+void TickIterCounter()
 {
-	// Checks the rows, columns, and 3x3 subgrids.
-	NumberTally tally;
-	for (int i = 0; i < 9; i++)
+	totalIter++;
+	if (++iter / incrSize > incr)
 	{
-		for (int j = 0; j < 9; j++)
+		if (incr > 0)
 		{
-			int value = inGrid.values[i][j];
-			if (value == 0)
+			for (int i = 0; i < log10(iter); i++)
 			{
-				continue;
+				cout << "\b";
 			}
-
-			tally.rowCount[i][value - 1]++;
-			tally.colCount[j][value - 1]++;
-			tally.subCount[GetBoxIndex(i, j)][value - 1]++;
 		}
+		incr = iter / incrSize;
+		cout << iter;
 	}
-
-	return tally;
 }
 
-void DrawGrid(const Grid9x9& grid)
+steady_clock::time_point startTime;
+
+inline void StartTimer()
 {
-	/*for (int i = 0; i < 23; i++)
-	{
-		cout << " ";
-	}
+	startTime = high_resolution_clock::now();
+}
 
-	for (int i = 0; i < 9; i++)
-	{
-		cout << " " << i + 1 << ":";
-	}
-	cout << endl;
-	
-	NumberTally tally = CountRepeats(grid);*/
-	for (int i1 = 0; i1 < 3; i1++)
-	{
-		for (int i2 = 0; i2 < 3; i2++)
-		{
-			int i = i1 * 3 + i2;
-			cout << "  ";
-			for (int j1 = 0; j1 < 3; j1++)
-			{
-				for (int j2 = 0; j2 < 3; j2++)
-				{
-					int value = grid.values[i][j1 * 3 + j2];
-					if (value == 0)
-					{
-						cout << "_ ";
-					}
-					else
-					{
-						cout << value << " ";
-					}
-				}
-				cout << " ";
-			}
-
-			/*cout << "\b";
-			for (int k = 0; k < 9; k++)
-			{
-				cout << "  " << tally.rowCount[i][k];
-			}*/
-			cout << endl;
-		}
-		cout << endl;
-	}
-
-	/*for (int i = 0; i < 23; i++)
-	{
-		cout << " ";
-	}
-
-	for (int i = 0; i < 9; i++)
-	{
-		cout << " s" << i << ":";
-	}
-	cout << endl;
-
-	for (int k = 0; k < 9; k++)
-	{
-		cout << k + 1 << ":";
-		for (int j1 = 0; j1 < 3; j1++)
-		{
-			for (int j2 = 0; j2 < 3; j2++)
-			{
-				cout << tally.colCount[j1 * 3 + j2][k] << " ";
-			}
-			cout << " ";
-		}
-
-		cout << "\b";
-		for (int ij = 0; ij < 9; ij++)
-		{
-			cout << "   " << tally.subCount[ij][k];
-		}
-		cout << endl;
-	}*/
+inline auto GetTimeSinceStart()
+{
+	return duration_cast<milliseconds>(high_resolution_clock::now() - startTime).count();
 }
 
 class ConstraintMatrix
 {
 	// There are 9 candidates for each of the 81 cells, represented by the rows.
 	// (e.g. a 5 at (2,3) is represented by its own row.)
-	
+
 	// There are 4 constraints represented by columns that each row satisfies:
 	// 1) One number for each of the 81 cells.
 	// 2) 9 unique numbers in each of the 9 rows.
 	// 3) 9 unique numbers in each of the 9 columns.
 	// 4) 9 unique numbers in each of the 9 boxes.
-	
+
 	// (e.g. 5 at (2,3) satisfies having 1) a number in cell (2,3), 2) a 5 in the 2nd row,
 	// 3) a 5 in the 3rd column, and 4) a 5 in the 1st box.)
-	
+
 private:
 	static const int rowCount = 9 * 81, colCount = 4 * 81;
+	array<int, rowCount> rowIndexer;
 	bool values[rowCount][colCount];
 
 	void DeleteRow(int r, int i, int j, int cand)
@@ -230,7 +109,7 @@ private:
 	}
 
 public:
-	ConstraintMatrix() : values{}
+	ConstraintMatrix() : rowIndexer {}, values {}
 	{
 		for (int i = 0; i < 9; i++)
 		{
@@ -244,10 +123,27 @@ public:
 				{
 					// Satisfies cell, row, column, and box constraints.
 					int r = pos * 9 + cand;
+					rowIndexer[r] = r;
 					values[r][pos] = values[r][row + cand] = values[r][col + cand] = values[r][box + cand] = 1;
 				}
 			}
 		}
+	}
+
+	void Shuffle()
+	{
+		random_device rd;
+		mt19937 g(rd());
+
+		for (int pos = 0; pos < rowCount; pos += 9)
+		{
+			shuffle(rowIndexer.begin() + pos, rowIndexer.begin() + pos + 9, g);
+		}
+	}
+
+	int GetCandidate(int r)
+	{
+		return rowIndexer[r] % 9;
 	}
 
 	void DeleteRowsColumns(int pos, int i1, int j1, int cand1)
@@ -273,7 +169,7 @@ public:
 		}
 	}
 
-	void Print(int posLen, int constrLen)
+	void Print(int posLen, int constrLen) const
 	{
 		for (int r1 = 0; r1 < posLen; r1++)
 		{
@@ -305,47 +201,125 @@ public:
 
 	const bool& operator()(int i, int j) const
 	{
+		return values[rowIndexer[i]][j];
+	}
+
+	bool& operator()(int i, int j)
+	{
+		return values[rowIndexer[i]][j];
+	}
+};
+
+class Grid9x9
+{
+private:
+	int values[9][9];
+	ConstraintMatrix matrix;
+
+public:
+	Grid9x9() : values{}, matrix()
+	{
+	}
+
+	void Generate()
+	{
+		int shuffles = totalIter = 0;
+		auto start = high_resolution_clock::now();
+		do
+		{
+			StartTimer();
+			iter = incr = 0;
+			cout << endl << "Shuffle " << ++shuffles << ": ";
+
+			matrix.Shuffle();
+		}
+		while (!XSolver(matrix, *this));
+		auto end = high_resolution_clock::now();
+
+		cout << endl;
+		cout << "Total iterations: " << totalIter << endl;
+		cout << "Total time: " << duration_cast<milliseconds>(end - start).count() << "ms" << endl;
+		cout << endl;
+	}
+
+	void Draw() const
+	{
+		for (int i1 = 0; i1 < 3; i1++)
+		{
+			for (int i2 = 0; i2 < 3; i2++)
+			{
+				int i = i1 * 3 + i2;
+				cout << "  ";
+				for (int j1 = 0; j1 < 3; j1++)
+				{
+					for (int j2 = 0; j2 < 3; j2++)
+					{
+						int value = values[i][j1 * 3 + j2];
+						if (value == 0)
+						{
+							cout << "_ ";
+						}
+						else
+						{
+							cout << value << " ";
+						}
+					}
+					cout << " ";
+				}
+				cout << endl;
+			}
+			cout << endl;
+		}
+	}
+
+	const int& operator()(int i, int j) const
+	{
 		return values[i][j];
 	}
-	
-	bool& operator()(int i, int j)
+
+	int& operator()(int i, int j)
 	{
 		return values[i][j];
 	}
 };
 
-int iter = 0, totalIter = 0, incr = 0, incrSize = 1000;
-void TickIterCounter()
+struct NumberTally
 {
-	totalIter++;
-	if (++iter / incrSize > incr)
+	int rowCount[9][9], colCount[9][9], subCount[9][9];
+	NumberTally() : rowCount{ 0 }, colCount{ 0 }, subCount{ 0 }
+	{}
+};
+
+inline void GetGridIndices(int inPos, int& outI, int& outJ)
+{
+	outJ = inPos % 9;
+	outI = (inPos - outJ) / 9;
+}
+
+NumberTally CountRepeats(Grid9x9 inGrid)
+{
+	// Checks the rows, columns, and 3x3 subgrids.
+	NumberTally tally;
+	for (int i = 0; i < 9; i++)
 	{
-		if (incr > 0)
+		for (int j = 0; j < 9; j++)
 		{
-			for (int i = 0; i < log10(iter); i++)
+			int value = inGrid(i, j);
+			if (value == 0)
 			{
-				cout << "\b";
+				continue;
 			}
+
+			tally.rowCount[i][value - 1]++;
+			tally.colCount[j][value - 1]++;
+			tally.subCount[GetBoxIndex(i, j)][value - 1]++;
 		}
-		incr = iter / incrSize;
-		cout << iter;
 	}
+
+	return tally;
 }
 
-steady_clock::time_point startTime;
-
-inline void StartTimer()
-{
-	startTime = high_resolution_clock::now();
-}
-
-inline long GetTimeSinceStart()
-{
-	return duration_cast<milliseconds>(high_resolution_clock::now() - startTime).count();
-}
-
-bool XSolver(ConstraintMatrix& matrix, Grid9x9& grid, const array<int, 9 * 81>& rowIndexer, int timeOut = 300,
-	int pos = 0, int r1 = 0)
+bool XSolver(ConstraintMatrix& matrix, Grid9x9& grid, int timeOut, int pos, int r1)
 {
 	if (pos > 80) // If all cells filled, exact-cover problem is solved so terminate successfully.
 	{
@@ -365,7 +339,7 @@ bool XSolver(ConstraintMatrix& matrix, Grid9x9& grid, const array<int, 9 * 81>& 
 	for (; r1 < endOfCands; r1++)
 	{
 		// Chooses a random candidate for a cell.
-		if (matrix(rowIndexer[r1], pos))
+		if (matrix(r1, pos))
 		{
 			isSuccessful = true;
 			break;
@@ -380,13 +354,13 @@ bool XSolver(ConstraintMatrix& matrix, Grid9x9& grid, const array<int, 9 * 81>& 
 	// Adds chosen candidate to the grid.
 	int i, j;
 	GetGridIndices(pos, i, j);
-	int cand = rowIndexer[r1] % 9;
-	grid.values[i][j] = cand + 1;
+	int cand = matrix.GetCandidate(r1);
+	grid(i, j) = cand + 1;
 
 	ConstraintMatrix* mat1 = new ConstraintMatrix(matrix);
 	mat1->DeleteRowsColumns(pos, i, j, cand);
 
-	if (XSolver(*mat1, grid, rowIndexer, timeOut, pos + 1))
+	if (XSolver(*mat1, grid, timeOut, pos + 1))
 	{
 		delete mat1;
 		return true;
@@ -396,7 +370,7 @@ bool XSolver(ConstraintMatrix& matrix, Grid9x9& grid, const array<int, 9 * 81>& 
 	// If the solver fails, then we must choose a different candidate for this position.
 	for (r1++; r1 < endOfCands; r1++)
 	{
-		if (XSolver(matrix, grid, rowIndexer, timeOut, pos, r1))
+		if (XSolver(matrix, grid, timeOut, pos, r1))
 		{
 			return true;
 		}
@@ -409,12 +383,6 @@ bool XSolver(ConstraintMatrix& matrix, Grid9x9& grid, const array<int, 9 * 81>& 
 int main()
 {
 	Grid9x9 grid;
-	array<int, 9 * 81> rowIndexer{ };
-	for (int r = 0; r < rowIndexer.size(); r++)
-	{
-		rowIndexer[r] = r;
-	}
-
 	while (true)
 	{
 		cout << "Generate new grid? [y/n]" << endl;
@@ -427,34 +395,8 @@ int main()
 			break;
 		}
 
-		ConstraintMatrix matrix;
-
-		int shuffles = totalIter = 0;
-		auto start = high_resolution_clock::now();
-		do
-		{
-			iter = incr = 0;
-			cout << endl << "Shuffle " << ++shuffles << ": ";
-
-			random_device rd;
-			mt19937 g(rd());
-
-			for (int pos = 0; pos < 81; pos++)
-			{
-				shuffle(rowIndexer.begin() + pos * 9, rowIndexer.begin() + pos * 9 + 9, g);
-			}
-
-			StartTimer();
-		}
-		while (!XSolver(matrix, grid, rowIndexer));
-		auto end = high_resolution_clock::now();
-
-		cout << endl;
-		cout << "Total iterations: " << totalIter << endl;
-		cout << "Total time: " << duration_cast<milliseconds>(end - start).count() << "ms" << endl;
-		cout << endl;
-
-		DrawGrid(grid);
+		grid.Generate();
+		grid.Draw();
 	}
 }
 
